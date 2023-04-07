79b18a9ca28cb2b193af9b6f5faa6b71215c8e28
Shamiivan <shamiivan@gmail.com>
2023-04-06 22:19:47 -0400

    add method to print all threads

--- src/ca/concordia/coen346/server/ProcessServer.java
+++ src/ca/concordia/coen346/server/ProcessServer.java
@@ -3,6 +3,7 @@ package ca.concordia.coen346.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
 
 public class ProcessServer {
 
@@ -14,7 +15,10 @@ public class ProcessServer {
             ServerSocket socket = new ServerSocket(8000);
 
             while(true) {
-                System.out.println("Server waiting for connections");
+                System.out.println("Thread : " +Thread.currentThread().getName() + " -- Server waiting for connections");
+
+                printListOfThreads();
+//                System.out.println("Server waiting for connections");
                 Socket client = socket.accept();
                 if (simulator.createProcess(client) == -1) {
                     client.sendUrgentData(-1);
@@ -27,5 +31,24 @@ public class ProcessServer {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    // print all the threads and relevant information about them
+    static void printListOfThreads(){
+        Map<Thread, StackTraceElement[]> threads = Thread.getAllStackTraces();
+        for (Thread thread : threads.keySet()) {
+            if(thread.getName().equals("OS simulator") || thread.getName().equals("main")){
+                System.out.println("Thread name: " + thread.getName());
+                System.out.println("Thread ID: " + thread.getId());
+                System.out.println("Thread state: " + thread.getState());
+                System.out.println("Thread stack trace: ");
+                StackTraceElement[] stackTrace = threads.get(thread);
+                for (StackTraceElement element : stackTrace) {
+                    System.out.println("\t" + element);
+                }
+                System.out.println();
+            }
+        }
+
     }
 }
