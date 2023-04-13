003ef92b16b5641a594b1db4292472499be2d4f9
Shamiivan <shamiivan@gmail.com>
2023-04-13 14:51:57 -0400

    Refactor the run function
    change the for loop into a timed loop
    to implement the concept of quantum

--- src/ca/concordia/coen346/server/Process.java
+++ src/ca/concordia/coen346/server/Process.java
@@ -1,70 +1,38 @@
 package ca.concordia.coen346.server;
 
-import ca.concordia.coen346.client.ConsumerClient;
-import ca.concordia.coen346.client.ProducerClient;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.Scanner;
 
-public class Process {
+public class Process implements Runnable{
 
     public final static String NUM_ITEMS = "getNumItems";
     public final static String GET_ITEM = "getItemInPos";
     public final static String NEXT_ITEM_POS = "getNextItemPos";
     public final static String TERMINATE = "terminate";
 
-    private int processId;
+    private final int processId;
 
     private Buffer buffer;
     private BufferedReader reader;
     private PrintWriter writer;
+    private int quantum;
+    private boolean toBeTerminated;
+
 
     public Process(int id, Socket socket, Buffer buffer) throws Exception{
         this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         this.writer = new PrintWriter(socket.getOutputStream(), true);
         this.buffer = buffer;
         this.processId = id;
+        this.toBeTerminated = false;
         // writing the PID to the client
         writer.println(processId);
 
         System.out.println("Process " + processId + " is created");
     }
 
-    // TODO: 2023-04-06 read and write to buffer 
-    public int run(int times){
-        System.out.println("Process " + getPID() + " is schedulled");
-
-        for(int i = 0; i < times; i++) {
-            try{
-                //msg to send to the client
-                String msg = "Process with ID: " + processId + " is scheduled to run on thread " + Thread.currentThread().getName();
-                sendMessage(msg);
-
-                String instruction = reader.readLine();
-                System.out.println(instruction);
-
-                if(instruction.equals(NUM_ITEMS)){
-                    int numItems = buffer.size();
-                    writer.println(numItems);
-                }
-                else if(instruction.equals(GET_ITEM)){
-                    int item = buffer.getNextItem();
-                    writer.println(item);
-                }
-                else if(instruction.equals(NEXT_ITEM_POS)){
-                    int index = buffer.getNextPosition();
-                    writer.println(index);
-                }
-                else if(instruction.equals(TERMINATE)){
-                    return -1;
-                }
-            }catch(IOException e){
-                return -1;
-            }
-        }
-        return 0;
-    }
+    // TODO: 2023-04-06 read and write to buffer
 
 
     // todo : implement the getNextItem Position 
@@ -90,5 +58,51 @@ public class Process {
     // function to write messages to client
     public void sendMessage(String message){
         writer.println(message);
+    }
+
+
+    @Override
+    public void run() {
+        System.out.println("Process " + getPID() + " is scheduled");
+
+        long startTime = System.currentTimeMillis();
+        long endTime = startTime + this.quantum;
+
+        // run the bloc for the time specified by quantum
+        while(System.currentTimeMillis() < endTime){
+            try{
+                //msg to send to the client
+                String msg = "Process with ID: " + processId + " is scheduled to run on thread " + Thread.currentThread().getName();
+                sendMessage(msg);
+
+                String instruction = reader.readLine();
+                System.out.println(instruction);
+
+                if(instruction.equals(NUM_ITEMS)){
+                    int numItems = buffer.size();
+                    writer.println(numItems);
+                }
+                else if(instruction.equals(GET_ITEM)){
+                    int item = buffer.getNextItem();
+                    writer.println(item);
+                }
+                else if(instruction.equals(NEXT_ITEM_POS)){
+                    int index = buffer.getNextPosition();
+                    writer.println(index);
+                }
+                else if(instruction.equals(TERMINATE)){
+                }
+            }catch(IOException e){
+            }
+        }
+
+    }
+
+
+    public void setQuantum(int quantum){
+        this.quantum = quantum;
+    }
+    public boolean toBeTerminated(){
+        return toBeTerminated();
     }
 }
